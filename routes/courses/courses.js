1feaaf3cefa7f354f26f5bd88e499c611677bfa9
Ibeneme <macbook@Ibeneme-Ikennas.local>
2024-07-15 14:28:36 +0100

    production updates

--- routes/courses/courses.js
+++ routes/courses/courses.js
@@ -1,7 +1,9 @@
 // routes/courseRoutes.js
 const express = require('express');
 const Course = require('../../models/Providers/Courses');
+const User = require('../../models/Users');
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 // POST route to create a new course
 router.post('/', async (req, res) => {
@@ -74,6 +76,43 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     console.error('Error deleting course:', err);
     res.status(500).json({ error: 'Failed to delete course' });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password, "email, password");
+    // Check if email and password are provided
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Compare provided password with hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Email or password is incorrect" });
+    }
+
+    // Delete the user from the database
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "Account deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting account:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
